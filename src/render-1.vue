f72de902f013ad47141ba8b53e40e7c5d7910e6a
morrxy <morrxy@gmail.com>
2019-06-13 14:36:25 +0800

    add a render example

---
+++ src/render-1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <anchorded-heading :level="1">Hello world!</anchorded-heading>
+    <anchorded-heading :level="2">Hello world!</anchorded-heading>
+    <anchorded-heading :level="3">Hello world!</anchorded-heading>
+  </div>
+</template>
+
+<script>
+const heading = {
+  props: {
+    level: {
+      type: Number,
+      required: true
+    }
+  },
+  render: function(createElement) {
+    return createElement('h' + this.level, this.$slots.default);
+  }
+};
+
+export default {
+  components: {
+    'anchorded-heading': heading
+  }
+};
+</script>
