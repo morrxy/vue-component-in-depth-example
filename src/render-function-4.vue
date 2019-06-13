9e4c1a429ec0bcf06b266a510ebf74f3624a2e5f
morrxy <morrxy@gmail.com>
2019-06-13 17:55:07 +0800

    add a render function example

---
+++ src/render-function-4.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <my-input :value="value" @input="handleInput" />
+    {{ value }}
+  </div>
+</template>
+
+<script>
+const myComponent = {
+  props: ['value'],
+  render: function(createElement) {
+    const vm = this;
+    return createElement('input', {
+      domProps: {
+        value: vm.value
+      },
+      on: {
+        input: function(event) {
+          vm.$emit('input', event.target.value);
+        }
+      }
+    });
+  }
+};
+
+export default {
+  components: {
+    'my-input': myComponent
+  },
+  data() {
+    return {
+      value: 'initValue'
+    };
+  },
+  methods: {
+    handleInput(val) {
+      this.value = val;
+    }
+  }
+};
+</script>
