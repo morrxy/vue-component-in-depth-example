926e776563bdf04b1c7a6f07d7ca4d9d8f8fc7dc
morrxy <morrxy@gmail.com>
2019-06-13 18:08:56 +0800

    add a render function example

---
+++ src/render-function-5.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <my-component>hello world!</my-component>
+  </div>
+</template>
+
+<script>
+const myComponent = {
+  render: function(createElement) {
+    // `<div><slot></slot></div>`
+    return createElement('div', this.$slots.default);
+  }
+};
+
+export default {
+  components: {
+    'my-component': myComponent
+  }
+};
+</script>
