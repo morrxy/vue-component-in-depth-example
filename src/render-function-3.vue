b563771a4a4995c9141e3ac5cfddaeac55631126
morrxy <morrxy@gmail.com>
2019-06-13 16:23:48 +0800

    add a render function example

---
+++ src/render-function-3.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <my-component :items="items1" />
+    <my-component :items="items2" />
+  </div>
+</template>
+
+<script>
+const comp = {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  render: function(createElement) {
+    if (this.items.length) {
+      return createElement(
+        'ul',
+        this.items.map(function(item) {
+          return createElement('li', item.name);
+        })
+      );
+    } else {
+      return createElement('p', 'No items found');
+    }
+  }
+};
+
+export default {
+  components: {
+    'my-component': comp
+  },
+  data() {
+    return {
+      items1: [
+        {
+          name: 'name1'
+        },
+        {
+          name: 'name2'
+        }
+      ],
+      items2: []
+    };
+  }
+};
+</script>
