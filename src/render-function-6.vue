9088681b56bda6843e402d3f2ab21c6f9bd28f49
morrxy <morrxy@gmail.com>
2019-06-14 17:26:54 +0800

    add 3 render function example

---
+++ src/render-function-6.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <my-component :message="message" v-slot:default="{ text }">
+      {{ text }}
+    </my-component>
+  </div>
+</template>
+
+<script>
+const myComponent = {
+  props: ['message'],
+  render: function(createElement) {
+    // `<div><slot :text="message"></slot></div>`
+    return createElement('div', [
+      this.$scopedSlots.default({
+        text: this.message
+      })
+    ]);
+  }
+};
+
+export default {
+  components: {
+    'my-component': myComponent
+  },
+  data() {
+    return {
+      message: 'hello world!'
+    };
+  }
+};
+</script>
