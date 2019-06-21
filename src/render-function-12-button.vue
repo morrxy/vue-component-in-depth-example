92d9aec3c8b13eeeb95d320da572b153624a21dd
morrxy <morrxy@gmail.com>
2019-06-21 11:01:23 +0800

    add some render function example

---
+++ src/render-function-12-button.vue
@@ -0,0 +1,5 @@
+<template functional>
+  <button class="btn btn-primary" v-bind="data.attrs" v-on="listeners">
+    <slot />
+  </button>
+</template>
---
+++ src/render-function-12.vue
@@ -0,0 +1,30 @@
+<script>
+import myButton from './render-function-12-button';
+
+export default {
+  components: {
+    'my-button': myButton
+  },
+
+  render(h) {
+    return h(
+      'my-button',
+      {
+        attrs: {
+          id: 'myButton',
+          class: 'myclass',
+          style: {
+            color: 'orange'
+          }
+        },
+        on: {
+          click: function() {
+            alert('clicked');
+          }
+        }
+      },
+      'my button'
+    );
+  }
+};
+</script>
